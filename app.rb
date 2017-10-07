8f2071926272799f9e8d7389ac327ec9629514d3
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-07 13:52:36 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/pig' do
+    @text = params[:user_text]
+    erb :pig
+  end
 end
